5f3ad78ddf07d116d58415ad574a3ce8ca860d3c
Artem Kolinichenko <habr@users.noreply.github.com>
2018-12-21 10:36:43 +0500

    Update 1.py
    Данное тестовое задание предназначено для выявление следующих навыков:
    • Умение работать самостоятельно;
    • Скорость выполнения задач;
    • Работа с современными технологиями.
    Во время выполнения тестового задания важно по максимуму проявить эти качества.
    Удачи!
    Необходимо:
    1) Использовать Python/Java/C#, подключить библиотеку Selenium Webdriver;
    2) С помощью Selenium открыть браузер, открыть gmail.com, авторизоваться, зайти на
    почту;
    3) В списке писем найти письма от Филинина Ильи (использовать поиск,
    предусмотренный самой почтой);
    4) с помощью Selenium определить, сколько нашлось писем;
    5) С помощью Selenium и интерфейса почты автоматически написать и отправить
    письмо Филинину Илье, в тексте которого указать найденное в шаге 4 количество
    писем.
    Указать тему письма "Тестовое задание. <Фамилия>", где <Фамилия> - это Ваша
    фамилия;
    При выполнении работы необходимо использовать следующие технологии:
    1) Selenium GRID (тесты запускать через GRID обязательно)
    2) Использовать паттерн проектирования автотестов Page Object
    3) Реализовать формирование отчетов о пройденных тестах через Allure

--- 1.py
+++ 1.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+# -*- encoding: utf-8 -*-
+#import sys
+#reload(sys)
+#sys.setdefaultencoding('utf8')
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import time
@@ -10,41 +11,54 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
-driver = webdriver.Chrome('/Users/Kobalt/Downloads/chromedriver')  # Optional argument, if not specified will search path.
-driver.get("http://www.yandex.ru")
-driver.find_element_by_class_name("desk-notif-card__login-enter-expanded").click()
-driver.find_element_by_name("login").send_keys("79225593111")
+class HomePage:
+    timeout = 10
+    loginVar="79225593111"
+    passwordVar="**************"
+    passwordField='//*[@id="passp-field-passwd"]'
+    mailSearch="girok66@mail.ru"
+    sendTo="kobalt18@gmail.com"
+    desiredCapabilities = {"browserName": "chrome"}
+    driver = webdriver.Remote(desired_capabilities=desiredCapabilities)
 
-timeout=10
+    def enter_in_mail(self):
+        driver = self.driver
+        driver.get("http://www.yandex.ru")
+        driver.find_element_by_class_name("desk-notif-card__login-enter-expanded").click()
+    def login_in_mail(self):
+        driver = self.driver
+        driver.find_element_by_name("login").send_keys(self.loginVar)
+        t = driver.find_element_by_name("passwd")
+        if t.is_displayed():
+            t.send_keys(self.passwordVar)
+            driver.find_element_by_class_name("passport-Button").click()
+        else:
+            driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div[2]/div/div/div[1]/form/div[3]/button[1]').click()
+            WebDriverWait(driver, self.timeout).until(EC.presence_of_element_located((By.XPATH,(self.passwordField))))
+            driver.find_element_by_xpath(self.passwordField).send_keys(self.passwordVar)
+            driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div[2]/div/div/div/form/div[2]/button').click()
+    def search_mail(self):
+        driver = self.driver
+        WebDriverWait(driver, self.timeout).until(EC.presence_of_element_located((By.XPATH,('/html/body/div[3]/div[5]/div/div[2]/div[3]/div[2]/div/div/span/input'))))
+        driver.find_element_by_class_name('textinput__control').send_keys(self.mailSearch)
+        driver.find_element_by_class_name('textinput__control').send_keys(u'\ue007')
+        WebDriverWait(driver, self.timeout).until(EC.presence_of_element_located((By.XPATH,('/html/body/div[3]/div[5]/div/div[3]/div[3]/div[2]/div[5]/div[1]/div/div/div[2]/div/div[1]/div/span/span'))))
+        printvar=driver.find_element_by_xpath('/html/body/div[3]/div[5]/div/div[3]/div[3]/div[2]/div[5]/div[1]/div/div/div[2]/div/div[1]/div/span/span').text
+        return str(printvar)
+    def write_send_mail(self, strprintvar):
+        driver = self.driver
+        driver.find_element_by_class_name('mail-ComposeButton').click()
+        WebDriverWait(driver, self.timeout).until(EC.presence_of_element_located((By.XPATH,('/html/body/div[3]/div[5]/div/div[3]/div[3]/div[2]/div[5]/div/div[1]/div[2]/div[1]/div/div[1]/label/div[3]/div'))))
+        driver.find_element_by_xpath('/html/body/div[3]/div[5]/div/div[3]/div[3]/div[2]/div[5]/div/div[1]/div[2]/div[1]/div/div[1]/label/div[3]/div').send_keys(self.sendTo)
+        driver.find_element_by_xpath('/html/body/div[3]/div[5]/div/div[3]/div[3]/div[2]/div[5]/div/div[1]/div[2]/div[1]/div/label/div[3]/input').send_keys("Тестовое задание. Колиниченко")
+        driver.find_element_by_class_name('cke_wysiwyg_div').send_keys("Найдено писем:" + strprintvar)
+    def exit(self):
+        driver = self.driver
+        driver.quit();
 
-t = driver.find_element_by_name("passwd")
-if t.is_displayed():
-    t.send_keys("password")
-    driver.find_element_by_class_name("passport-Button").click()
-else:
-    driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div[2]/div/div/div[1]/form/div[3]/button[1]').click()
-    WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH,('//*[@id="passp-field-passwd"]'))))
-    driver.find_element_by_xpath('//*[@id="passp-field-passwd"]').send_keys("password")
-    driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div[2]/div/div/div/form/div[2]/button').click()
-
-WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH,('//*[@id="nb-1"]/body/div[2]/div[5]/div/div[2]/div[3]/div[2]/div/div/span/input'))))
-driver.find_element_by_class_name('textinput__control').send_keys("girok66@mail.ru")
-driver.find_element_by_class_name('textinput__control').send_keys(u'\ue007')
-
-WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH,('//*[@id="nb-1"]/body/div[2]/div[5]/div/div[3]/div[3]/div[2]/div[5]/div[1]/div/div/div[2]/div/div[1]/div/span/span'))))
-
-printvar=driver.find_element_by_xpath('//*[@id="nb-1"]/body/div[2]/div[5]/div/div[3]/div[3]/div[2]/div[5]/div[1]/div/div/div[2]/div/div[1]/div/span/span').text
-driver.find_element_by_xpath('//*[@id="nb-1"]/body/div[2]/div[5]/div/div[2]/div[2]/div/div/a').click()
-
-WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH,('//*[@id="nb-1"]/body/div[2]/div[5]/div/div[3]/div[3]/div[2]/div[5]/div/div[1]/div[2]/div[1]/div/div[1]/label/div[3]/div'))))
-driver.find_element_by_xpath('//*[@id="nb-1"]/body/div[2]/div[5]/div/div[3]/div[3]/div[2]/div[5]/div/div[1]/div[2]/div[1]/div/div[1]/label/div[3]/div').send_keys("kobalt18@gmail.com")
-
-driver.find_element_by_xpath('//*[@id="nb-1"]/body/div[2]/div[5]/div/div[3]/div[3]/div[2]/div[5]/div/div[1]/div[2]/div[1]/div/label/div[3]/input').send_keys(u"Тестовое задание. Колиниченко")
-
-
-
-driver.find_element_by_class_name('cke_wysiwyg_div').send_keys("Найдено писем:" + printvar)
-
-driver.find_element_by_xpath('//*[@id="nb-18"]').click()
-
-
+class RunTest:
+    HomePage().enter_in_mail()
+    HomePage().login_in_mail()
+    strPrint=HomePage().search_mail()
+    HomePage().write_send_mail(strPrint)
+    HomePage().exit()
